2ceee342ba14edb90526318be7263d50831de0aa
artick788 <60552965+artick788@users.noreply.github.com>
2025-11-08 18:50:27 +0100

    Extended Velyra enum up to 128

--- include/VelyraUtils/VelyraEnum.hpp
+++ include/VelyraUtils/VelyraEnum.hpp
@@ -10,22 +10,29 @@
 #include <spdlog/fmt/bundled/format.h>
 
 #define VA_COUNT_IMPL( \
-_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, \
+_1,_2,_3,_4,_5,_6,_7,_8,_9,_10, \
 _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
 _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
 _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
 _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
 _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
-_61,_62,_63,N,...) N
+_61,_62,_63,_64,_65,_66,_67,_68,_69,_70, \
+_71,_72,_73,_74,_75,_76,_77,_78,_79,_80, \
+_81,_82,_83,_84,_85,_86,_87,_88,_89,_90, \
+_91,_92,_93,_94,_95,_96,_97,_98,_99,_100, \
+_101,_102,_103,_104,_105,_106,_107,_108,_109,_110, \
+_111,_112,_113,_114,_115,_116,_117,_118,_119,_120, \
+_121,_122,_123,_124,_125,_126,_127,_128, N, ...) N
 
 #define VA_COUNT(...) VA_COUNT_IMPL(__VA_ARGS__, \
-63,62,61,60,                   \
-59,58,57,56,55,54,53,52,51,50,\
-49,48,47,46,45,44,43,42,41,40,\
-39,38,37,36,35,34,33,32,31,30,\
-29,28,27,26,25,24,23,22,21,20,\
-19,18,17,16,15,14,13,12,11,10,\
-9,8,7,6,5,4,3,2,1,0)
+128,127,126,125,124,123,122,121,120,119,118,117,116,115,114,113, \
+112,111,110,109,108,107,106,105,104,103,102,101,100,99,98,97, \
+96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81, \
+80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65, \
+64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49, \
+48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33, \
+32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17, \
+16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
 
 
 #define VL_ENUM_PP_MAP(macro, data, ...)                         \
@@ -102,11 +109,76 @@ _61,_62,_63,N,...) N
 #define VL_ENUM_M62(m, d, x, ...) m(d, 61, x) VL_ENUM_ID(VL_ENUM_M61(m, d, __VA_ARGS__))
 #define VL_ENUM_M63(m, d, x, ...) m(d, 62, x) VL_ENUM_ID(VL_ENUM_M62(m, d, __VA_ARGS__))
 #define VL_ENUM_M64(m, d, x, ...) m(d, 63, x) VL_ENUM_ID(VL_ENUM_M63(m, d, __VA_ARGS__))
+#define VL_ENUM_M65(m, d, x, ...) m(d, 64, x) VL_ENUM_ID(VL_ENUM_M64(m, d, __VA_ARGS__))
+#define VL_ENUM_M66(m, d, x, ...) m(d, 65, x) VL_ENUM_ID(VL_ENUM_M65(m, d, __VA_ARGS__))
+#define VL_ENUM_M67(m, d, x, ...) m(d, 66, x) VL_ENUM_ID(VL_ENUM_M66(m, d, __VA_ARGS__))
+#define VL_ENUM_M68(m, d, x, ...) m(d, 67, x) VL_ENUM_ID(VL_ENUM_M67(m, d, __VA_ARGS__))
+#define VL_ENUM_M69(m, d, x, ...) m(d, 68, x) VL_ENUM_ID(VL_ENUM_M68(m, d, __VA_ARGS__))
+#define VL_ENUM_M70(m, d, x, ...) m(d, 69, x) VL_ENUM_ID(VL_ENUM_M69(m, d, __VA_ARGS__))
+#define VL_ENUM_M71(m, d, x, ...) m(d, 70, x) VL_ENUM_ID(VL_ENUM_M70(m, d, __VA_ARGS__))
+#define VL_ENUM_M72(m, d, x, ...) m(d, 71, x) VL_ENUM_ID(VL_ENUM_M71(m, d, __VA_ARGS__))
+#define VL_ENUM_M73(m, d, x, ...) m(d, 72, x) VL_ENUM_ID(VL_ENUM_M72(m, d, __VA_ARGS__))
+#define VL_ENUM_M74(m, d, x, ...) m(d, 73, x) VL_ENUM_ID(VL_ENUM_M73(m, d, __VA_ARGS__))
+#define VL_ENUM_M75(m, d, x, ...) m(d, 74, x) VL_ENUM_ID(VL_ENUM_M74(m, d, __VA_ARGS__))
+#define VL_ENUM_M76(m, d, x, ...) m(d, 75, x) VL_ENUM_ID(VL_ENUM_M75(m, d, __VA_ARGS__))
+#define VL_ENUM_M77(m, d, x, ...) m(d, 76, x) VL_ENUM_ID(VL_ENUM_M76(m, d, __VA_ARGS__))
+#define VL_ENUM_M78(m, d, x, ...) m(d, 77, x) VL_ENUM_ID(VL_ENUM_M77(m, d, __VA_ARGS__))
+#define VL_ENUM_M79(m, d, x, ...) m(d, 78, x) VL_ENUM_ID(VL_ENUM_M78(m, d, __VA_ARGS__))
+#define VL_ENUM_M80(m, d, x, ...) m(d, 79, x) VL_ENUM_ID(VL_ENUM_M79(m, d, __VA_ARGS__))
+#define VL_ENUM_M81(m, d, x, ...) m(d, 80, x) VL_ENUM_ID(VL_ENUM_M80(m, d, __VA_ARGS__))
+#define VL_ENUM_M82(m, d, x, ...) m(d, 81, x) VL_ENUM_ID(VL_ENUM_M81(m, d, __VA_ARGS__))
+#define VL_ENUM_M83(m, d, x, ...) m(d, 82, x) VL_ENUM_ID(VL_ENUM_M82(m, d, __VA_ARGS__))
+#define VL_ENUM_M84(m, d, x, ...) m(d, 83, x) VL_ENUM_ID(VL_ENUM_M83(m, d, __VA_ARGS__))
+#define VL_ENUM_M85(m, d, x, ...) m(d, 84, x) VL_ENUM_ID(VL_ENUM_M84(m, d, __VA_ARGS__))
+#define VL_ENUM_M86(m, d, x, ...) m(d, 85, x) VL_ENUM_ID(VL_ENUM_M85(m, d, __VA_ARGS__))
+#define VL_ENUM_M87(m, d, x, ...) m(d, 86, x) VL_ENUM_ID(VL_ENUM_M86(m, d, __VA_ARGS__))
+#define VL_ENUM_M88(m, d, x, ...) m(d, 87, x) VL_ENUM_ID(VL_ENUM_M87(m, d, __VA_ARGS__))
+#define VL_ENUM_M89(m, d, x, ...) m(d, 88, x) VL_ENUM_ID(VL_ENUM_M88(m, d, __VA_ARGS__))
+#define VL_ENUM_M90(m, d, x, ...) m(d, 89, x) VL_ENUM_ID(VL_ENUM_M89(m, d, __VA_ARGS__))
+#define VL_ENUM_M91(m, d, x, ...) m(d, 90, x) VL_ENUM_ID(VL_ENUM_M90(m, d, __VA_ARGS__))
+#define VL_ENUM_M92(m, d, x, ...) m(d, 91, x) VL_ENUM_ID(VL_ENUM_M91(m, d, __VA_ARGS__))
+#define VL_ENUM_M93(m, d, x, ...) m(d, 92, x) VL_ENUM_ID(VL_ENUM_M92(m, d, __VA_ARGS__))
+#define VL_ENUM_M94(m, d, x, ...) m(d, 93, x) VL_ENUM_ID(VL_ENUM_M93(m, d, __VA_ARGS__))
+#define VL_ENUM_M95(m, d, x, ...) m(d, 94, x) VL_ENUM_ID(VL_ENUM_M94(m, d, __VA_ARGS__))
+#define VL_ENUM_M96(m, d, x, ...) m(d, 95, x) VL_ENUM_ID(VL_ENUM_M95(m, d, __VA_ARGS__))
+#define VL_ENUM_M97(m, d, x, ...) m(d, 96, x) VL_ENUM_ID(VL_ENUM_M96(m, d, __VA_ARGS__))
+#define VL_ENUM_M98(m, d, x, ...) m(d, 97, x) VL_ENUM_ID(VL_ENUM_M97(m, d, __VA_ARGS__))
+#define VL_ENUM_M99(m, d, x, ...) m(d, 98, x) VL_ENUM_ID(VL_ENUM_M98(m, d, __VA_ARGS__))
+#define VL_ENUM_M100(m, d, x, ...) m(d, 99, x) VL_ENUM_ID(VL_ENUM_M99(m, d, __VA_ARGS__))
+#define VL_ENUM_M101(m, d, x, ...) m(d, 100, x) VL_ENUM_ID(VL_ENUM_M100(m, d, __VA_ARGS__))
+#define VL_ENUM_M102(m, d, x, ...) m(d, 101, x) VL_ENUM_ID(VL_ENUM_M101(m, d, __VA_ARGS__))
+#define VL_ENUM_M103(m, d, x, ...) m(d, 102, x) VL_ENUM_ID(VL_ENUM_M102(m, d, __VA_ARGS__))
+#define VL_ENUM_M104(m, d, x, ...) m(d, 103, x) VL_ENUM_ID(VL_ENUM_M103(m, d, __VA_ARGS__))
+#define VL_ENUM_M105(m, d, x, ...) m(d, 104, x) VL_ENUM_ID(VL_ENUM_M104(m, d, __VA_ARGS__))
+#define VL_ENUM_M106(m, d, x, ...) m(d, 105, x) VL_ENUM_ID(VL_ENUM_M105(m, d, __VA_ARGS__))
+#define VL_ENUM_M107(m, d, x, ...) m(d, 106, x) VL_ENUM_ID(VL_ENUM_M106(m, d, __VA_ARGS__))
+#define VL_ENUM_M108(m, d, x, ...) m(d, 107, x) VL_ENUM_ID(VL_ENUM_M107(m, d, __VA_ARGS__))
+#define VL_ENUM_M109(m, d, x, ...) m(d, 108, x) VL_ENUM_ID(VL_ENUM_M108(m, d, __VA_ARGS__))
+#define VL_ENUM_M110(m, d, x, ...) m(d, 109, x) VL_ENUM_ID(VL_ENUM_M109(m, d, __VA_ARGS__))
+#define VL_ENUM_M111(m, d, x, ...) m(d, 110, x) VL_ENUM_ID(VL_ENUM_M110(m, d, __VA_ARGS__))
+#define VL_ENUM_M112(m, d, x, ...) m(d, 111, x) VL_ENUM_ID(VL_ENUM_M111(m, d, __VA_ARGS__))
+#define VL_ENUM_M113(m, d, x, ...) m(d, 112, x) VL_ENUM_ID(VL_ENUM_M112(m, d, __VA_ARGS__))
+#define VL_ENUM_M114(m, d, x, ...) m(d, 113, x) VL_ENUM_ID(VL_ENUM_M113(m, d, __VA_ARGS__))
+#define VL_ENUM_M115(m, d, x, ...) m(d, 114, x) VL_ENUM_ID(VL_ENUM_M114(m, d, __VA_ARGS__))
+#define VL_ENUM_M116(m, d, x, ...) m(d, 115, x) VL_ENUM_ID(VL_ENUM_M115(m, d, __VA_ARGS__))
+#define VL_ENUM_M117(m, d, x, ...) m(d, 116, x) VL_ENUM_ID(VL_ENUM_M116(m, d, __VA_ARGS__))
+#define VL_ENUM_M118(m, d, x, ...) m(d, 117, x) VL_ENUM_ID(VL_ENUM_M117(m, d, __VA_ARGS__))
+#define VL_ENUM_M119(m, d, x, ...) m(d, 118, x) VL_ENUM_ID(VL_ENUM_M118(m, d, __VA_ARGS__))
+#define VL_ENUM_M120(m, d, x, ...) m(d, 119, x) VL_ENUM_ID(VL_ENUM_M119(m, d, __VA_ARGS__))
+#define VL_ENUM_M121(m, d, x, ...) m(d, 120, x) VL_ENUM_ID(VL_ENUM_M120(m, d, __VA_ARGS__))
+#define VL_ENUM_M122(m, d, x, ...) m(d, 121, x) VL_ENUM_ID(VL_ENUM_M121(m, d, __VA_ARGS__))
+#define VL_ENUM_M123(m, d, x, ...) m(d, 122, x) VL_ENUM_ID(VL_ENUM_M122(m, d, __VA_ARGS__))
+#define VL_ENUM_M124(m, d, x, ...) m(d, 123, x) VL_ENUM_ID(VL_ENUM_M123(m, d, __VA_ARGS__))
+#define VL_ENUM_M125(m, d, x, ...) m(d, 124, x) VL_ENUM_ID(VL_ENUM_M124(m, d, __VA_ARGS__))
+#define VL_ENUM_M126(m, d, x, ...) m(d, 125, x) VL_ENUM_ID(VL_ENUM_M125(m, d, __VA_ARGS__))
+#define VL_ENUM_M127(m, d, x, ...) m(d, 126, x) VL_ENUM_ID(VL_ENUM_M126(m, d, __VA_ARGS__))
+#define VL_ENUM_M128(m, d, x, ...) m(d, 127, x) VL_ENUM_ID(VL_ENUM_M127(m, d, __VA_ARGS__))
 
-#define VL_ENUM_PP_COUNT_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, count, ...) count
+
+#define VL_ENUM_PP_COUNT_IMPL(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64,_65,_66,_67,_68,_69,_70,_71,_72,_73,_74,_75,_76,_77,_78,_79,_80,_81,_82,_83,_84,_85,_86,_87,_88,_89,_90,_91,_92,_93,_94,_95,_96,_97,_98,_99,_100,_101,_102,_103,_104,_105,_106,_107,_108,_109,_110,_111,_112,_113,_114,_115,_116,_117,_118,_119,_120,_121,_122,_123,_124,_125,_126,_127,_128,count, ...) count
 
 #define VL_ENUM_PP_COUNT(...)                                                                                     \
-    VL_ENUM_ID(VL_ENUM_PP_COUNT_IMPL(__VA_ARGS__, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
+    VL_ENUM_ID(VL_ENUM_PP_COUNT_IMPL(__VA_ARGS__, 128,127,126,125,124,123,122,121,120,119,118,117,116,115,114,113,112,111,110,109,108,107,106,105,104,103,102,101,100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
 
 template<typename T>
 struct is_vl_enum: std::false_type {};
@@ -117,7 +189,7 @@ T fromString(const std::string& value){
     throw std::runtime_error("fromString not implemented for this type");
 }
 
-namespace ap::detail {
+namespace Velyra::Detail {
     /** Get intrinsic value of an (Enum::value) by taking advantage of
      * C-conversion's parentheses priority
      */
@@ -162,7 +234,7 @@ namespace ap::detail {
 
 #define VL_ENUM_ADD_ENTRY(EnumType, idx, expression) expression,
 #define VL_ENUM_ADD_VALUE(EnumType, idx, expression) \
-    (EnumType)((ap::detail::_eat_assign<EnumType>)EnumType::expression),
+    (EnumType)((Velyra::Detail::_eat_assign<EnumType>)EnumType::expression),
 #define VL_ENUM_VALUE_STRING(EnumType, idx, expression) \
     #expression,
 
@@ -202,7 +274,7 @@ namespace ap::detail {
         static void generateStructures(){ \
             for (std::size_t idx = 0; idx < VL_ENUM_VALUE_COUNT_NAME; idx++){ \
                 Name value = VL_ENUM_VALUE_ARRAY_NAME[idx]; \
-                std::string value_str = ap::detail::getEnumValue(VL_ENUM_RAW_NAMES_ARRAY_NAME[idx]); \
+                std::string value_str = Velyra::Detail::getEnumValue(VL_ENUM_RAW_NAMES_ARRAY_NAME[idx]); \
                 VL_ENUM_MVL_VALUE_TO_STR.insert({value, value_str}); \
                 VL_ENUM_MVL_STR_TO_VALUE.insert({value_str, value}); \
             } \
